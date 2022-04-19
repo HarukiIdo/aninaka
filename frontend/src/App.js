2b4c215ccad745dcfccbda88eb95bc9c43fdbc63
kiyo <kiyo.a328@gmail.com>
2022-04-19 09:40:49 +0900

    ログイン機能の追加

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./routes/Home";
 import Layout from "./components/Layout";
+import useFirebaseAuth from "./auth/useFirebaseAuth";
+import Login from "./auth/Login";
+import SignUp from "./auth/SignUp";
 
 const App = () => {
+  const { isAuthenticated, isLoading, currentUser } = useFirebaseAuth();
+  if (isLoading) return <p>loading...</p>;
   return (
     <>
       <Layout>
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route
+            path="/"
+            element={isAuthenticated ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/signup" element={<SignUp />} />
         </Routes>
       </Layout>
     </>
